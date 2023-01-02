31f56bb40d9af928cfdafd59275b42185e87e2ea
sunokryu <sunokryu319@gmail.com>
2023-01-02 23:28:43 +0900

    mbti 오늘 자기 전에 끝내자

--- js/apps.js
+++ js/apps.js
@@ -1,88 +1,78 @@
-const mbtiInfo = {
-  istp: [
-    "대담하면서도 현실적인 성격으로, 모든 종류의 도구를 자유자재로 다루는 장인입니다.",
-    "ISTP",
-  ],
-  istj: ["사실을 중시하는 믿음직한 현실주의자입니다.", , "ISTJ"],
-  isfp: [
-    "항상 새로운 경험을 추구하는 유연하고 매력 넘치는 예술가입니다.",
-    "ISFP",
-  ],
-  isfj: [
-    "주변 사람을 보호할 준비가 되어있는 헌신적이고 따뜻한 수호자입니다.",
-    "ISFJ",
-  ],
-  intp: ["지식을 끝없이 갈망하는 혁신적인 발명가입니다.", "INTP"],
-  intj: ["모든 일에 대해 계획을 세우며 상상력이 풍부한 전략가입니다.", "INTJ"],
-  infp: [
-    "항상 선을 행할 준비가 되어 있는 부드럽고 친절한 이타주의자입니다.",
-    "INFP",
-  ],
-  infj: [
-    "차분하고 신비한 분위기를 풍기는 성격으로, 다른 사람에게 의욕을 불어넣는 이상주의자입니다.",
-    "INFJ",
-  ],
-  estp: [
-    "위험을 기꺼이 감수하는 성격으로 영리하고 에너지 넘치며 관찰력이 뛰어난 사업가입니다.",
-    "ESTP",
-  ],
-  estj: ["사물과 사람을 관리하는 데 뛰어난 능력을 지닌 경영자입니다.", "ESTJ"],
-  esfp: [
-    "즉흥적이고 넘치는 에너지와 열정으로 주변사람을 즐겁게 하는 연예인입니다.",
-    "ESFP",
-  ],
-  esfj: [
-    "배려심이 넘치고 항상 타인을 도울 준비가 되어있는 성격으로, 사교성이 높은 마당발입니다.",
-    "ESFJ",
-  ],
-  entp: ["지적 도전을 즐기는 영리하고 호기심이 많은 사색가입니다.", "ENTP"],
-  entj: [
-    "항상 해결책을 찾아내는 성격으로, 대담하고 상상력이 풍부하며 의지가 강한 지도자입니다.",
-    "ENTJ",
-  ],
-  enfp: [
-    "열정적이고 창의적인 성격으로, 긍정적으로 삶을 바라보는 사교적이면서도 자유로운 영혼입니다.",
-    "ENFP",
-  ],
-  enfj: [
-    "청중을 사로잡고 의욕을 불어넣는 카리스마 넘치는 지도자입니다.",
-    "ENFJ",
-  ],
-};
-let pageNumber = 1;
+import mbtiReulst from "./mbtiResult.js";
+import question from "./question.js";
+
 const imageContainer1 = document.querySelector("#image-container1");
 const imageContainer2 = document.querySelector("#image-container2");
 const imageContainer3 = document.querySelector("#image-container3");
-
 const textContainer = document.querySelector("#text-container");
 const leftItem = document.querySelector("#left-item");
 const rightItem = document.querySelector("#right-item");
-const HIDDEN_KEY = "hidden";
-
-let answerArr = [
-  null,
-  null,
-  null,
-  null,
-  null,
-  null,
-  null,
-  null,
-  null,
-  null,
-  null,
-  null,
-];
 const page = document.querySelector("#page");
-function onStart() {
+const before = document.querySelector("#before");
+const HIDDEN_KEY = "hidden";
+const CHECK_KEY = "value";
+let whatOfFour;
+let pageNumber = 1;
+let undefinedCheckArr = new Array(12);
+let finalResult = [0, 0, 0, 0];
+// orderIndex = [] 0~11 순서대로
+let orderIndex = [];
+for (let i = 0; i < question.length; i++) {
+  orderIndex.push(i);
+}
+// orderIndex = 0~11 랜덤 배열 -> for what ? : 질문 순서 랜덤하게 하려고
+orderIndex.sort(() => Math.random() - 0.5); // 뒤집인 index가 들어있는 orderIndexArr
+let pageIndexCorrelation = []; // for what ? : [pageNumber, orderIndex]
+for (let i = 1; i <= question.length; i < i++) {
+  const index = orderIndex[i - 1];
+  pageIndexCorrelation.push([
+    i,
+    question[index].surbey,
+    question[index].relatedLocationIndex,
+  ]); // pageIndexCorrelation : page, surbey, relatedLocationIndex
+}
+function printQuestion(page) {
+  // 매개변수로 pageNumber를 넣는다.
+  // 반환은 현재 질문 인덱스를 반환한다.
+  for (let i = 0; i < question.length; i++) {
+    if (pageIndexCorrelation[i][0] === page) {
+      leftItem.innerText = pageIndexCorrelation[i][1][0];
+      rightItem.innerText = pageIndexCorrelation[i][1][1];
+      whatOfFour = pageIndexCorrelation[i][2]; // 몇번째 인덱스에 값을 바꿔줄꺼냐
+      break;
+    }
+  }
+}
+function openContainer2() {
   imageContainer1.classList.add(HIDDEN_KEY);
   imageContainer2.classList.remove(HIDDEN_KEY);
   textContainer.classList.remove(HIDDEN_KEY);
+}
+function showPage() {
   page.innerText = `${pageNumber}/12`;
+}
+
+// 시작할 떄 -> 사진 변경
+function onStart() {
+  openContainer2();
+  printQuestion(pageNumber);
+  showPage();
 }
 imageContainer1.addEventListener("click", onStart);
 
-const before = document.querySelector("#before");
+
+
+// 카드 선택할 때 -> 
+function Click() {
+  if (this.getAttribute(CHECK_KEY) == 1) {
+    // 오른쪽 선택
+    finalResult[whatOfFour] -= 
+  } else {
+    // 왼쪽 선택
+  }
+}
+leftItem.addEventListener("click", Click);
+rightItem.addEventListener("click", Click);
 
 function pagingMinus() {
   if (pageNumber === 1) {
---
+++ js/question.js
@@ -0,0 +1,84 @@
+export default [
+  {
+    // e i
+    surbey: [
+      "휴일은 다른 사람들과 같이 지내는 게 좋다.",
+      "휴일은 혼자 보내는게 좋다.",
+    ],
+    relatedLocationIndex: 0,
+  },
+
+  {
+    surbey: [
+      "생각을 자연스럽게 나눌 때 브레인스토밍이 가장 잘 된다.",
+      "논제를 미리 알려주었을때 브레인스토밍이 가장 잘 된다.",
+    ],
+    relatedLocationIndex: 0,
+  },
+
+  {
+    surbey: ["어딜 가든 친구를 사귄다.", "진정한 친구가 몇 안된다."],
+    relatedLocationIndex: 0,
+  },
+
+  {
+    surbey: ["숲보다는 나무에 집중한다.", "나무보다는 숲에 집중한다."],
+    relatedLocationIndex: 1,
+  },
+  {
+    surbey: [
+      "오감에 의존하며 실존 정보, 기록, 자신의 경험 위주에 의존하여 판단을 내린다.",
+      "아이디어, 직관, 영감에 의존해서 판단을 내린다.",
+    ],
+    relatedLocationIndex: 1,
+  },
+  {
+    surbey: [
+      "몇 분동안 아무 생각 하지 않을 수 있다.",
+      "아무 생각을 하지 않는 것이 어렵다.",
+    ],
+    relatedLocationIndex: 1,
+  },
+  {
+    surbey: [
+      "객관적으로 사실을 판단한다.",
+      "상황적, 포괄적이며 주변 상황을 고려하여 판단한다.",
+    ],
+    relatedLocationIndex: 2,
+  },
+  {
+    surbey: [
+      "과정보다 결과에 초점을 맞춘다.",
+      "결과보다 과정에 초점을 맞춘다.",
+    ],
+    relatedLocationIndex: 2,
+  },
+  {
+    surbey: [
+      "고민상담을 할 때 주로 실질적인 해결책을 제시해주는 편이다.",
+      "고민상담을 할 때 주로 정서적인 지지와 공감을 해주는 편이다.",
+    ],
+    relatedLocationIndex: 2,
+  },
+
+  {
+    surbey: [
+      "유동적인 목적과 방향을 선호한다.",
+      "분명한 목적과 방향을 선호한다.",
+    ],
+    relatedLocationIndex: 3,
+  },
+
+  {
+    surbey: [
+      "반복되는 일상을 지겨워 한다.",
+      "반복되는 일상이 깨지면 스트레스를 받는다.",
+    ],
+    relatedLocationIndex: 3,
+  },
+
+  {
+    surbey: ["당일 약속이 싫지 않다.", "당일 약속이 싫다."],
+    relatedLocationIndex: 3,
+  },
+];
